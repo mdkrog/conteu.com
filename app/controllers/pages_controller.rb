0b94776da1a35dbc0dc7f8f799ea0b91721a5d46
Matthew Krog <mdkrog@gmail.com>
2017-06-04 12:26:06 +0200

    Absolute positioning of contents page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def example
+    render layout: 'contents_layout'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  get '/example' => 'pages#example', as: 'example'
 end
