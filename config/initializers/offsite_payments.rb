2c28099007ac3b1fbdf6db04774d0daab24bf401
Matthew Krog <mdkrog@gmail.com>
2017-07-16 23:40:57 +0200

    Setup Payfast Credentials

--- config/initializers/offsite_payments.rb
+++ config/initializers/offsite_payments.rb
@@ -1,6 +1,9 @@
 require 'offsite_payments'
 require 'offsite_payments/action_view_helper'
 
+template = ERB.new File.new("#{Rails.root}/config/payfast.yml.erb").read
+# processed = YAML.load template.result(binding)
+
 ActionView::Base.send(:include, OffsitePayments::ActionViewHelper)
 #OffsitePayments.mode = :test # for testing server
-Rails.configuration.offsite_payments = YAML.load_file("#{Rails.root}/config/application.yml")
+Rails.configuration.offsite_payments = YAML.load template.result(binding)
