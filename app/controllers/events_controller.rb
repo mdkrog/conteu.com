9863608d30fe01d341a1d2c1c79d3c2e8a5c23ca
Matthew Krog <mdkrog@gmail.com>
2018-07-12 13:55:09 +0200

    Allow index to be seen by public

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :require_login, except: [:show]
+  before_action :require_login, except: [:show, :index]
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
